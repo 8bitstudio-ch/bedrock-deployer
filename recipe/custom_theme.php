e7e50f5537735c2e3cbb6acf8df85ee51120e72b
Mathieu Zwygart <mathieu@zwygart.net>
2025-01-21 11:30:32 +0100

    remove theme build

--- recipe/custom_theme.php
+++ recipe/custom_theme.php
@@ -2,11 +2,6 @@
 
 namespace Deployer;
 
-desc( 'Compiles the theme locally for production' );
-task( 'custom_theme:compile', function () {
-    runLocally( "npm run prod" );
-} );
-
 desc( 'Updates remote assets with local assets, but without deleting previous assets on destination' );
 task( 'custom_theme:upload_assets_only', function () {
     upload("{{local_root}}/{{theme_path}}/", "{{release_path}}/{{theme_path}}");
@@ -14,6 +9,5 @@ task( 'custom_theme:upload_assets_only', function () {
 
 desc( 'Builds assets and uploads them on remote server' );
 task( 'push:custom_theme', [
-    'custom_theme:compile',
     'custom_theme:upload_assets_only',
 ] );
